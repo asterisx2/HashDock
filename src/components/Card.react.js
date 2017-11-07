831ef958caa837e6976500ba35aafb98440f1bf4
ashwin <ashwinsinghgr8@gmail.com>
2017-11-07 10:35:31 +0530

    Added index pages

--- src/components/Card.react.js
+++ src/components/Card.react.js
@@ -4,10 +4,10 @@ const Card =({tweet}) => {
     return (
         <div>
             <img src={tweet.avatar} className="tavatar"/>
-            <span className="tuser">tweet.user</span>
-            <a target="_blank" className="thandle"></a>
-            <span className="tDateTime">tweet.dateTime</span>
-            <p className="ttext">tweet.text</p>
+            <span className="tuser">{tweet.user}</span>
+            <a target="_blank" className="thandle" href="">@{tweet.handle}</a>
+            <span className="tDateTime">{tweet.dateTime}</span>
+            <p className="ttext">{tweet.text}</p>
         </div>
 );
 };
